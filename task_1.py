c60f86ab2e64d4f6415210d7d5c1b663dec4ef1b
Anna Sviridova <anitta-yar@yandex.ru>
2025-06-23 21:07:01 +0300

    выполнено задание 1

--- task_1.py
+++ task_1.py
@@ -1,16 +1,11 @@
 str='1h 45m,360s,25m,30m 120s,2h 60s'
-str_new=str.split(',')
-print(str_new)
-str_new2=[]
-for i in range(0,len(str_new)):
-    str_new2+=str_new[i].split(' ')
-print(str_new2)
+normalized_str=str.replace(" ",',').split(',')
 sum=0
-for i in range(0,(len(str_new2))):
-    if 'h' in str_new2[i]:
-        sum+=int(str_new2[i].replace('h',''))*60
-    elif 'm' in str_new2[i]:
-        sum+=int(str_new2[i].replace('m',''))
-    elif 's' in str_new2[i]:
-        sum+=int(int(str_new2[i].replace('s',''))/60)
+for i in normalized_str:
+    if 'h' in i:
+        sum+=int(i.replace('h',''))*60
+    elif 'm' in i:
+        sum+=int(i.replace('m',''))
+    elif 's' in i:
+        sum+=int(int(i.replace('s',''))/60)
 print(sum)
